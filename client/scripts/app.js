dd746cf0b58ed6261a13bf39a28b1e0ba25fd5f8
swfree <skye.free@gmail.com>
2016-03-15 11:47:18 -0700

    Fix bugs in room selection

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,32 +1,41 @@
 // YOUR CODE HERE:
 var app = {
+  server: 'https://api.parse.com/1/classes/messages',
+  currentRoom: 'all',
+
   init: function() {
+    // Initialize friends & call fetch once to fill page
     this.friends = {};
-    this.fetch();
-    console.log('hi');
+    this.fetch(function(obj) {
+      return obj.roomname === app.currentRoom;
+    });
+
+    // Click handler for adding usernames to friends list
     $(document).on('click', '.username', function(e) { 
       e.preventDefault();
       app.addFriend($(this)[0].innerText.substring(1));
     });
 
+    // Handler for submitting messages
     $(document).on('submit', '#postMessage', function(e) {
       e.preventDefault();
       var body = $(this).find('input[name="message"]').val();
       app.addMessage(body);
-      app.fetch();
+      app.fetch(function(obj) {
+        return obj.roomname === app.currentRoom;
+      });
     });
 
+    // Handler for changing room dropdown selection
     $(document).on('change', '#roomList', function() {
       // filter update for loops to only have $(this) roomnames
 
-      var roomCheck = this.value;
+      app.currentRoom = this.value;
       app.fetch(function(obj) {
-        return obj.roomname === roomCheck;
+        return obj.roomname === app.currentRoom;
       });
     });
   },
-
-  server: 'https://api.parse.com/1/classes/messages',
 
   send: function(message) {
     $.ajax({
@@ -44,12 +53,11 @@ var app = {
   },
 
   fetch: function(predicate) {
-    // TODO: only add newest; or add to top rather than compounding
-    // app.clearMessages();
     $.ajax({
       url: this.server,
       type: 'GET',
       success: function(data) {
+        // Update Messages:
         app.update(data, predicate);
       },
       error: function(data) {
@@ -66,8 +74,13 @@ var app = {
 
     // Sanitize user inputs for 30 most recent posts
     for (var i = 0; i < 30; i++) {
-      if (predicate && !predicate(data.results[i])) { continue; }
-      if (!data.results[i].username || !data.results[i].text || !data.results[i].roomname) { continue; }
+      // Check if roomname === currentRoom:
+      if (predicate && !predicate(data.results[i]) && app.currentRoom !== 'all') { 
+        continue; 
+      }
+      if (!data.results[i].username || !data.results[i].text || !data.results[i].roomname) { 
+        continue; 
+      }
 
       cleanUsers.push(data.results[i].username.replace(/(<([^>]+)>)/ig, "")); 
       cleanTexts.push(data.results[i].text.replace(/(<([^>]+)>)/ig, ""));
@@ -89,12 +102,22 @@ var app = {
           '<p>' + cleanTexts[i] + '</p></div>');
       }
     }
+    app.getRooms(data);
+  },
 
     // Add rooms to rooms dropdown
-    var currentRoom = $('#roomList')[0].value || 'all';
-    $('#roomList').html('<option value="' + currentRoom + '">' + currentRoom + '</option>');
+    // Collect all rooms currently in all server messages
+    // populate select dropdown with those rooms
+  getRooms: function(data) {
+    $('#roomList').html('');
+    var rooms = [];
+    for (var i = 0; i < data.results.length; i++) {
+      if (data.results[i].roomname) {
+        rooms.push(data.results[i].roomname.replace(/(<([^>]+)>)/ig, ""));
+      }
+    }
     rooms = _.uniq(rooms, false);
-    var option = '';
+    var option = '<option value="all">all</option>';
     for (var i = 0; i < rooms.length; i++) {
       option += '<option value="' + rooms[i] + '">' + rooms[i] + '</option>';
     }
@@ -124,25 +147,18 @@ var app = {
     if (!app.friends.hasOwnProperty(user)) {
       app.friends[user] = user;
       console.log(app.friends[user]);
-      app.fetch();
+      app.fetch(function(obj) {
+        return obj.roomname === app.currentRoom;
+      });
     }
   },
-
-  handleSubmit: function() {}
 };
 
 app.init();
 
-app.fetch();
 setInterval(function() {
-  app.fetch();  
+  app.fetch(function(obj) {
+    return obj.roomname === app.currentRoom;
+  });  
 }, 3000);
-
-
-
-
-
-
-
-
 
