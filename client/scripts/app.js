f44d834fb5246cbbcc4a01adc04b06e9f30fbf9c
swfree <skye.free@gmail.com>
2016-03-15 11:07:45 -0700

    Add room dropdown

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -4,7 +4,8 @@ var app = {
     this.friends = {};
     this.fetch();
     console.log('hi');
-    $(document).on('click', '.username', function() { 
+    $(document).on('click', '.username', function(e) { 
+      e.preventDefault();
       app.addFriend($(this)[0].innerText.substring(1));
     });
 
@@ -12,6 +13,16 @@ var app = {
       e.preventDefault();
       var body = $(this).find('input[name="message"]').val();
       app.addMessage(body);
+      app.fetch();
+    });
+
+    $(document).on('change', '#roomList', function() {
+      // filter update for loops to only have $(this) roomnames
+
+      var roomCheck = this.value;
+      app.fetch(function(obj) {
+        return obj.roomname === roomCheck;
+      });
     });
   },
 
@@ -32,36 +43,62 @@ var app = {
     });
   },
 
-  fetch: function() {
+  fetch: function(predicate) {
     // TODO: only add newest; or add to top rather than compounding
     // app.clearMessages();
     $.ajax({
       url: this.server,
       type: 'GET',
       success: function(data) {
-        for (var i = 0; i < 30; i++) {
-          if (!data.results[i].username || !data.results[i].text) {
-            continue;
-          }
-          var cleanUser = data.results[i].username.replace(/(<([^>]+)>)/ig, ""); 
-          var cleanText = data.results[i].text.replace(/(<([^>]+)>)/ig, ""); 
-          if (app.friends.hasOwnProperty(cleanUser)) {
-            $('#chats').append(
-              '<div class="chat">' + 
-              '<a href="#" class="username">@' + cleanUser + '</a>' + 
-              '<p><b>' + cleanText + '</b></p></div>');
-          } else {
-            $('#chats').append(
-              '<div class="chat">' + 
-              '<a href="#" class="username">@' + cleanUser + '</a>' + 
-              '<p>' + cleanText + '</p></div>');
-          }
-        } 
+        app.update(data, predicate);
       },
       error: function(data) {
         console.error('chatterbox: Failed to fetch messages', data);
       }
     });
+  },
+
+  update: function(data, predicate) {
+    var $chats = $('#chats');
+    var cleanUsers = [];
+    var cleanTexts = [];
+    var rooms = [];
+
+    // Sanitize user inputs for 30 most recent posts
+    for (var i = 0; i < 30; i++) {
+      if (predicate && !predicate(data.results[i])) { continue; }
+      if (!data.results[i].username || !data.results[i].text || !data.results[i].roomname) { continue; }
+
+      cleanUsers.push(data.results[i].username.replace(/(<([^>]+)>)/ig, "")); 
+      cleanTexts.push(data.results[i].text.replace(/(<([^>]+)>)/ig, ""));
+      rooms.push(data.results[i].roomname.replace(/(<([^>]+)>)/ig, ""));
+    }
+
+    $chats.html('');
+    // Append sanitized user inputs to chats DOM element
+    for (var i = 0; i < cleanUsers.length; i++) {
+      if (app.friends.hasOwnProperty(cleanUsers[i])) {
+        $('#chats').append(
+          '<div class="chat">' + 
+          '<a href="#" class="username">@' + cleanUsers[i] + '</a>' + 
+          '<p><b>' + cleanTexts[i] + '</b></p></div>');
+      } else {
+        $('#chats').append(
+          '<div class="chat">' + 
+          '<a href="#" class="username">@' + cleanUsers[i] + '</a>' + 
+          '<p>' + cleanTexts[i] + '</p></div>');
+      }
+    }
+
+    // Add rooms to rooms dropdown
+    var currentRoom = $('#roomList')[0].value || 'all';
+    $('#roomList').html('<option value="' + currentRoom + '">' + currentRoom + '</option>');
+    rooms = _.uniq(rooms, false);
+    var option = '';
+    for (var i = 0; i < rooms.length; i++) {
+      option += '<option value="' + rooms[i] + '">' + rooms[i] + '</option>';
+    }
+    $('#roomList').append(option);
   },
  
   clearMessages: function() {
@@ -70,7 +107,7 @@ var app = {
 
   addMessage: function(message) {
     var currentUsername = window.location.search.substring(10); 
-    var roomName = 'stuff';
+    var roomName = $('#roomList')[0].value;
     var sendMessage = {
       username: currentUsername,
       text: message,
@@ -100,36 +137,6 @@ app.fetch();
 setInterval(function() {
   app.fetch();  
 }, 3000);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
